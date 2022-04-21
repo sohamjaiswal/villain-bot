b0a2c25a76bb4b2138baddeeff36a117d3dbb395
WLVF <65689785+WLVF@users.noreply.github.com>
2022-04-21 13:55:26 -0400

    4 space indents pog

--- src/utils/urban.ts
+++ src/utils/urban.ts
@@ -2,10 +2,10 @@ import ud from 'urban-dictionary'
 
 export const define: (word: string) => Promise<ud.DefinitionObject | null> = async(word: string) => {
     return ud.define(word).then((results) => {      
-         const definition = results[0]
-         console.log(definition)
-         return definition
-      }).catch((error) => {
-          return null
-      })
+        const definition = results[0]
+        console.log(definition)
+        return definition
+    }).catch((error) => {
+        return null
+    })
 }
