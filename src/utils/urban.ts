6a69bfc578ddd4286cf88b68e0294f29baea691a
Soham Jaiswal <sohamjaiswal10@gmail.com>
2022-04-22 12:08:30 +0530

    chore: removed console log

--- src/utils/urban.ts
+++ src/utils/urban.ts
@@ -3,7 +3,6 @@ import ud from 'urban-dictionary'
 export const define: (word: string) => Promise<ud.DefinitionObject | null> = async (word: string) => {
     return ud.define(word).then((results) => {
         const definition = results[0]
-        console.log(definition)
         return definition
     }).catch((error) => {
         return null
