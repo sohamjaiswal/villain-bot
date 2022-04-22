fb065a88b44af8d333cf31d531f5c98f0f3e095d
Soham Jaiswal <sohamjaiswal10@gmail.com>
2022-04-22 11:45:53 +0530

    fix: prefix

--- src/index.ts
+++ src/index.ts
@@ -22,40 +22,42 @@ if (token && prefix) {
         if (message.content === "test") {
             return message.reply("test indeed");
         }
-        const [commandName, ...args] = message.content.slice(prefix.length).split(" ");
-        switch (commandName) {
-            // case "image": {
-            //     uploadImage('2000.jpg').then((res) => res)
-            //     break
-            //     }
-            // case "static": {
-            //     await message.send("https://s3-us-west-2.amazonaws.com/www.guilded.gg/ContentMedia/7ea74857963de8735850cfb51cc9d0d4-Full.webp?w=680&h=933")
-            //     break
-            // }
-            case "help": {
-                await message.send("Yo! \n TooMuchHam made me! \n Ask him about my features as I am in active dev.")
-                break
-            }
-            case "urban": {
-                const words = [args].join(" ")
-                const definition: DefinitionObject | null = await define(words)
-                if (definition) {
-                    const msgContent: string = `word: ${definition.word} \n definition: ${definition.definition}`
-                    await message.send(msgContent)
-                } else {
-                    await message.send("Oof! I tried, but couldn't get the meaning...")
+        if (message.content.startsWith(prefix)) {
+            const [commandName, ...args] = message.content.slice(prefix.length).split(" ");
+            switch (commandName) {
+                // case "image": {
+                //     uploadImage('2000.jpg').then((res) => res)
+                //     break
+                //     }
+                // case "static": {
+                //     await message.send("https://s3-us-west-2.amazonaws.com/www.guilded.gg/ContentMedia/7ea74857963de8735850cfb51cc9d0d4-Full.webp?w=680&h=933")
+                //     break
+                // }
+                case "help" : {
+                    await message.send("Yo! \n TooMuchHam made me! \n Ask him about my features as I am in active dev.")
+                    break
                 }
-                break
-            }
-            case "invite": {
-                await message.reply("Here is my invite link! \n https://www.guilded.gg/b/35d4f270-489b-4088-9c9c-dc1e3f986ede")
-                break
-            }
-            case "rant": {
-                await message.reply("ham good, haters idiots")
-            }
-            case "shrug": {
-                await message.reply("¯\\_(ツ)_/¯")
+                case "urban" : {
+                    const words = [args].join(" ")
+                    const definition: DefinitionObject | null = await define(words)
+                    if (definition) {
+                        const msgContent: string = `word: ${definition.word} \n definition: ${definition.definition}`
+                        await message.send(msgContent)
+                    } else {
+                        await message.send("Oof! I tried, but couldn't get the meaning...")
+                    }
+                    break
+                }
+                case "invite" : {
+                    await message.reply("Here is my invite link! \n https://www.guilded.gg/b/35d4f270-489b-4088-9c9c-dc1e3f986ede")
+                    break
+                }
+                case "rant": {
+                    await message.reply("ham good, haters idiots")
+                }
+                case "shrug": {
+                    await message.reply("¯\\_(ツ)_/¯")
+                }
             }
         }
     });
